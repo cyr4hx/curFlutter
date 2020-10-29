011d9333150c7350224a74197f365631c00ab471
cyr4hx <46264822+cyr4hx@users.noreply.github.com>
2020-10-29 11:34:48 -0300

    Create dartTemplate.dart

---
+++ dartTemplate.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp( Ceo() );
+
+class Ceo extends StatelessWidget
+{
+  @override
+  Widget build( BuildContext context )
+  {
+    return MateriallApp
+           (
+             home: Scaffold
+                   (
+                     appBar: AppBar
+                             (
+                               
+                             ),
+                     drawer: Drawer
+                             (
+                               
+                             ),
+                     body: Column
+                             (
+                               
+                             ),
+                   ),
+           );
+  }
+}
